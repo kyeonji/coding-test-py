564c9e9254c51f4252251acd261650f62719aa58
kkyeonji <gjbernice@gmail.com>
2023-07-01 10:46:46 +0900

    '23. 7. 1. [programmers] 스킬트리

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import programmers_parking_fee_calculation as pfc
 import programmers_hopscotch as h
 import programmers_visiting_length as vl
+import programmers_skill_tree as st
 
 if __name__ == '__main__':
-    dirs = "ULURRDLLU"
+    skill = "CBD"
+    skill_trees = ["BACDE", "CBADF", "AECB", "BDA"]
 
-    answer = vl.solution(dirs)
+    answer = st.solution(skill, skill_trees)
     print(answer)
---
+++ programmers_skill_tree.py
@@ -0,0 +1,26 @@
+def solution(skill, skill_trees):
+    answer = 0
+
+    for skill_tree in skill_trees:
+        skill_to_check = ""
+        for s in skill_tree:
+            # in : if element exist in list or string
+            if s in skill:
+                skill_to_check += s
+        # list.find(e) : return index of e in list. if do not exits, return -1
+        # list. index(e) : do same but, if do not exits, occurs error
+        if skill.find(skill_to_check) == 0:
+            answer += 1
+
+    return answer
+
+# [learned]
+# 1. initialize list with 0 of specific length
+#    l = [0] * length
+# 2. can be implemented with for-end
+#    if for loop ended with break -> just break
+#    if for loop ended without break -> do sth. in else
+#    don't need check flag!
+# 3. list.pop(0)
+#    queue can be implemented with list in python
+#    list.pop(0) return list[0] and also pop() that element = pop_left
